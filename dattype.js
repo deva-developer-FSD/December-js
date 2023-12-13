bf95a5e41205e47e655e325e1aba6260a8904a7d
Devendhran S <103272706+deva-developer-FSD@users.noreply.github.com>
2023-12-13 10:04:40 -0800

    Add files via upload

---
+++ dattype.js
@@ -0,0 +1,30 @@
+// primitive data type
+
+var num = 10; 
+var name ='deva'; 
+var percentage = 6.5; 
+var flag = true; 
+ 
+console.log(num,typeof(num));
+console.log(name,typeof(name));
+console.log(percentage,typeof(percentage));
+console.log(flag,typeof(flag));
+
+var deva="123";
+console.log(deva);
+
+// nonprimitive data type
+
+// array =>Collection of data types
+const arr=[10,"deva",6.5,false];
+console.log(arr);
+
+// object JSON=> javascript Object Notation
+// key and Value pair
+const obj ={
+    student:'deva',
+    batch:'B54WE Tamil',
+    started: true,
+    arr:[4,3,"d"],
+};
+console.log(obj);
