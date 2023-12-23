d1cb0c388efa5c3d911368ff34e950efb005fe34
Devendhran S <103272706+deva-developer-FSD@users.noreply.github.com>
2023-12-23 09:25:39 -0800

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,34 @@
+// string types
+
+//length,uppercase or lowercase
+// charAt
+// subString
+// split
+// indexOf
+// replace
+// trim
+
+var a ="23";
+var b ="24-1";
+var c ="23";
+
+// if(a===b){
+//     console.log("not equal");
+// }
+// else if (a===c){
+// console.log("iam right");
+// }
+// else {
+//     console.log("game over");
+// }
+
+if (a===c){
+    console.log("iam busy");
+}
+else if (b==c){
+    console.log("iam happy");
+}
+else {
+    console.log(`very happy`);
+}
+
