61c8f9a367ca9b968095622609cb57075f683dd4
Devendhran S <103272706+deva-developer-FSD@users.noreply.github.com>
2023-12-24 09:00:12 -0800

    Add files via upload

---
+++ complex.js
@@ -0,0 +1,32 @@
+var arr= ["1,2,3,4,5,6,7,8,9,......100"];
+arr[50]=deva;
+console.log(arr.length);
+
+// var details={
+//     name:`deva`,
+//     age:23,
+//     year:2001,
+//     add:`trichy`,
+// };
+// for(var a in details){
+// console.log(a,";",details[a]);
+// }
+
+// var myName = {
+//     firstname:`deva`,
+//     middlename:`raj`,
+//     lastname:`kumar`,
+// };
+// for(var boss in myName){
+//     console.log(boss ,`-`,myName[boss]);
+// }
+
+var deva = {
+    firstname:`raja`,
+    age:23,
+    dad:`siva`,
+    since:2001,
+};
+for (var bose in deva){
+    console.log(bose,":",deva [bose]);
+}
